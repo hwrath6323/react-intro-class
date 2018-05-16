95267d2fe9f11e1912a7fb781bceea074d942890
Hannah W <newuser@New-User-6.local>
2018-05-15 18:05:36 -0600

    Through Step 5

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 import Calculator from './components/Calculator/Calculator';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <Calculator />
+        <Calculator />
       </div>
     );
   }
